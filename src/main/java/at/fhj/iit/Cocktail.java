c4fb20a59ea31e2d32e4231379d168b8b558f957
vinziw <vinzenz.wratschko@edu.campus02.at>
2020-05-09 19:04:15 +0200

    Preparing methods and variables with JavaDoc in Cocktails

--- src/main/java/at/fhj/iit/Cocktail.java
+++ src/main/java/at/fhj/iit/Cocktail.java
@@ -1,7 +1,49 @@
 package at.fhj.iit;
 
 
-public class Cocktail {
+public class Cocktail{
+
+    /**
+     *
+     * @param shaken
+     * @param name
+     * @param liquidType
+     * @param ingredients
+     * constructor with parameters from class
+     */
+
+
+    /**
+     * method for printing out the ingredients of a cocktail
+     * @param ingredients
+     */
+
+    /**
+     * @param shaken
+     * method, which asks the user, if is a fan of James Bond
+     * exception if not shaken
+     */
+
+
+    /**
+     *
+     * @return volume of the Drink
+     * Overriding getvolume() method
+     */
+
+
+    /**
+     *
+     * @return percentage of the Drink
+     * Overriding getAlcoholPercent() method
+     */
+
+
+    /**
+     *
+     * @return true , if drink is alcholic
+     * Overriding isAlcoholic() method
+     */
 
 }
 
