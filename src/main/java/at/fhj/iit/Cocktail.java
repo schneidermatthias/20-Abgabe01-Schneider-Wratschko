b80c14da1ac5d646eb48349c6ff54a35bc0b2a71
vinziw <vinzenz.wratschko@edu.campus02.at>
2020-05-10 11:11:53 +0200

    JamesBondException &methods added

--- src/main/java/at/fhj/iit/Cocktail.java
+++ src/main/java/at/fhj/iit/Cocktail.java
@@ -1,49 +1,72 @@
 package at.fhj.iit;
 
-
 import java.util.ArrayList;
+
+/**
+ * Class represents a Cocktail which extends drinks
+ * @author Matthias
+ * @author Vinzen
+ * @version 1.1
+ */
 
 public class Cocktail extends Drink {
 
-	
-
-	/**
-	 * @author Matthias
-	 * @author Vinzen
-	 * @version 1.0
-	 */
-
-	boolean alcoholic;
+	boolean shaken;
 	Liquid liquidType;
 	ArrayList<String> ingredients = new ArrayList<String>();
+	boolean bigCockatil;
 
 	/**
 	 *
-	 * @param shaken
-	 * @param name
-	 * @param liquidType
-	 * @param ingredients constructor with parameters from class
+	 * @param shaken describes if cocktail is shaken
+	 * @param name name of the cockatil
+	 * @param liquidType represents an Object of Liquid
+	 * @param ingredients List with ingredients of cocktail
+     * @param bigCockatil either a big (1l) or small (0.5l)cocktail
+     * constructor with parameters from class
 	 */
-
-	public Cocktail(String name, Liquid liquidType, boolean alcoholic, ArrayList ingredients) {
+	
+	public Cocktail(String name, Liquid liquidType, boolean shaken, ArrayList<String> ingredients,boolean bigCockatil) {
 		super(name);
 		this.name = name;
 		this.liquidType = liquidType;
-		this.alcoholic = alcoholic;
+		this.shaken = shaken;
 		this.ingredients= ingredients;
-		
+		this.bigCockatil = bigCockatil;
 	}
 
 	/**
 	 * method for printing out the ingredients of a cocktail
 	 * 
-	 * @param ingredients
 	 */
+
+	public void printIngredients(){
+        for (int i = 0; i < ingredients.size(); i++) {
+            System.out.println(ingredients.get(i));
+        }
+
+    }
 
 	/**
 	 * @param shaken method, which asks the user, if is a fan of James Bond
-	 *               exception if not shaken
+     * @throws JamesBondException if not shaken
 	 */
+	public void shakenNotStirred(boolean shaken) throws JamesBondException{
+	    try {
+	        if(!shaken){
+	            throw new JamesBondException("Mr. Bond wouldn't be satisfied");
+            }
+	        else{
+                System.out.println("Like the good old Mr. Bond");
+
+            }
+
+        }
+	    catch (JamesBondException ignored){
+
+        }
+
+    }
 
 	/**
 	 *
@@ -51,7 +74,7 @@ public class Cocktail extends Drink {
 	 */
 	@Override
 	public double getVolume() {
-		return 0;
+		return bigCockatil?1d:0.5d;
 	}
 
 	/**
---
+++ src/main/java/at/fhj/iit/JamesBondException.java
@@ -0,0 +1,7 @@
+package at.fhj.iit;
+
+public class JamesBondException extends Exception {
+    public JamesBondException(String message) {
+        super(message);
+    }
+}
