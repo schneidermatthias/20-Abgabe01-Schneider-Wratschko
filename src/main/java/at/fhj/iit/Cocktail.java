cdc79d32e2f8c0405c520cd052e06d816d3312b6
Matthias <62705122+schneidermatthias@users.noreply.github.com>
2020-05-10 10:39:04 +0200

    creating constructor and overriding abstract methods

--- src/main/java/at/fhj/iit/Cocktail.java
+++ src/main/java/at/fhj/iit/Cocktail.java
@@ -1,49 +1,78 @@
 package at.fhj.iit;
 
 
-public class Cocktail{
+import java.util.ArrayList;
 
-    /**
-     *
-     * @param shaken
-     * @param name
-     * @param liquidType
-     * @param ingredients
-     * constructor with parameters from class
-     */
+public class Cocktail extends Drink {
 
+	
 
-    /**
-     * method for printing out the ingredients of a cocktail
-     * @param ingredients
-     */
+	/**
+	 * @author Matthias
+	 * @author Vinzen
+	 * @version 1.0
+	 */
 
-    /**
-     * @param shaken
-     * method, which asks the user, if is a fan of James Bond
-     * exception if not shaken
-     */
+	boolean alcoholic;
+	Liquid liquidType;
+	ArrayList<String> ingredients = new ArrayList<String>();
 
+	/**
+	 *
+	 * @param shaken
+	 * @param name
+	 * @param liquidType
+	 * @param ingredients constructor with parameters from class
+	 */
 
-    /**
-     *
-     * @return volume of the Drink
-     * Overriding getvolume() method
-     */
+	public Cocktail(String name, Liquid liquidType, boolean alcoholic, ArrayList ingredients) {
+		super(name);
+		this.name = name;
+		this.liquidType = liquidType;
+		this.alcoholic = alcoholic;
+		this.ingredients= ingredients;
+		
+	}
 
+	/**
+	 * method for printing out the ingredients of a cocktail
+	 * 
+	 * @param ingredients
+	 */
 
-    /**
-     *
-     * @return percentage of the Drink
-     * Overriding getAlcoholPercent() method
-     */
+	/**
+	 * @param shaken method, which asks the user, if is a fan of James Bond
+	 *               exception if not shaken
+	 */
 
+	/**
+	 *
+	 * @return volume of the Drink Overriding getvolume() method
+	 */
+	@Override
+	public double getVolume() {
+		return 0;
+	}
 
-    /**
-     *
-     * @return true , if drink is alcholic
-     * Overriding isAlcoholic() method
-     */
+	/**
+	 *
+	 * @return percentage of the Drink Overriding getAlcoholPercent() method
+	 */
+	@Override
+	public double getAlcoholPercent() {
 
+		return liquidType.getAlcoholPercent();
+	}
+
+	/**
+	 *
+	 * @return true , if drink is alcholic Overriding isAlcoholic() method
+	 */
+	@Override
+	public boolean isAlcoholic() {
+
+		return liquidType.getAlcoholPercent() > 0;
+
+	}
 }
 
