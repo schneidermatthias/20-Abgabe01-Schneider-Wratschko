84dc988f9e17e24939528ea7320340f3b48cab85
vinziw <vinzenz.wratschko@edu.campus02.at>
2020-05-11 16:45:18 +0200

    imported package + List  to Cockatil Test

--- src/test/java/at/fhj/iit/CocktailTest.java
+++ src/test/java/at/fhj/iit/CocktailTest.java
@@ -1,16 +1,17 @@
-
+package at.fhj.iit;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
+import java.util.ArrayList;
 
 
 @DisplayName ("Testing Cocktail implementation")
 class CocktailTest {
 	
 	private Cocktail shake,stirred;
-    	private ArrayList<Liquid> liq1, liq2;
-    	private ArrayList<String> ing1 = new ArrayList<String>();
+	private ArrayList<Liquid> liq1, liq2;
+	private ArrayList<String> ing1 = new ArrayList<String>();
 
 	/**
 	 * inits liquid List and ingrediants List for each cocktail
@@ -18,12 +19,12 @@ class CocktailTest {
 	@BeforeEach
 	public void setup() {
 		liq1 = new ArrayList<>();
-        	liq1.add(new Liquid("Rum",0.25,20.5));
-        	liq1.add(new Liquid("Wasser",0.35,20.0));
-        	ing1.add("Lime");
-        	ing1.add("Sugar");
-        	ing1.add("Apple");
-        	shake = new Cocktail("Rum-Wasser",liq1,true,ing1,true);
+		liq1.add(new Liquid("Rum",0.25,20.5));
+		liq1.add(new Liquid("Wasser",0.35,20.0));
+		ing1.add("Lime");
+		ing1.add("Sugar");
+		ing1.add("Apple");
+		shake = new Cocktail("Rum-Wasser",liq1,true,ing1,true);
 		
 	}
 	
