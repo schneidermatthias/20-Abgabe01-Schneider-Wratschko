7e277b40a2a587fe078c0ef109810320c1a7ca73
vinziw <vinzenz.wratschko@edu.campus02.at>
2020-05-11 16:58:07 +0200

    preparing SimpleDRinkTest class

--- src/test/java/at/fhj/iit/CocktailTest.java
+++ src/test/java/at/fhj/iit/CocktailTest.java
@@ -8,48 +8,48 @@ import java.util.ArrayList;
 
 @DisplayName ("Testing Cocktail implementation")
 class CocktailTest {
-	
-	private Cocktail shake,stirred;
-	private ArrayList<Liquid> liq1, liq2;
-	private ArrayList<String> ing1 = new ArrayList<String>();
 
-	/**
-	 * inits liquid List and ingrediants List for each cocktail
-	 */
-	@BeforeEach
-	public void setup() {
-		liq1 = new ArrayList<>();
-		liq1.add(new Liquid("Rum",0.25,20.5));
-		liq1.add(new Liquid("Wasser",0.35,20.0));
-		ing1.add("Lime");
-		ing1.add("Sugar");
-		ing1.add("Apple");
-		shake = new Cocktail("Rum-Wasser",liq1,true,ing1,true);
-		
-	}
-	
-	/**
-	 * test the getIngredients method
-	 */
-	
-	
-	/**
-	 * test the shakenNotStirred method/exception
-	 */
-	
-	
-	/*
-	 * test the getVolume method
-	 */
-	
-	
-	/**
-	 * test the getAlcoholPercent method
-	 */
-	
-	/**
-	 * test the isAlcoholic method
-	 */
-	
-	
+    private Cocktail shake,stirred;
+    private ArrayList<Liquid> liq1, liq2;
+    private ArrayList<String> ing1 = new ArrayList<String>();
+
+    /**
+     * inits liquid List and ingrediants List for each cocktail
+     */
+    @BeforeEach
+    public void setup() {
+        liq1 = new ArrayList<>();
+        liq1.add(new Liquid("Rum",0.25,20.5));
+        liq1.add(new Liquid("Wasser",0.35,20.0));
+        ing1.add("Lime");
+        ing1.add("Sugar");
+        ing1.add("Apple");
+        shake = new Cocktail("Rum-Wasser",liq1,true,ing1,true);
+
+    }
+
+    /**
+     * test the getIngredients method
+     */
+
+
+    /**
+     * test the shakenNotStirred method/exception
+     */
+
+
+    /**
+     * test the getVolume method
+     */
+
+
+    /**
+     * test the getAlcoholPercent method
+     */
+
+    /**
+     * test the isAlcoholic method
+     */
+
+
 }
---
+++ src/test/java/at/fhj/iit/SimpleDrinkTest.java
@@ -0,0 +1,30 @@
+package at.fhj.iit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleDrinkTest {
+
+
+    /**
+     * Test getVolume method
+     */
+    @Test
+    void getVolume() {
+    }
+
+    /**
+     * Test getAlcoholPercent method
+     */
+    @Test
+    void getAlcoholPercent() {
+    }
+
+    /**
+     * Test isAlcohlic method
+     */
+    @Test
+    void isAlcoholic() {
+    }
+}
