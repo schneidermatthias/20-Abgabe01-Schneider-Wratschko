abfc5a2c5eda5e4224ca98bd400e17843dfee723
Matthias <62705122+schneidermatthias@users.noreply.github.com>
2020-05-12 19:19:05 +0200

    Finished SimpleDrinkTest with 100%

--- src/test/java/at/fhj/iit/SimpleDrinkTest.java
+++ src/test/java/at/fhj/iit/SimpleDrinkTest.java
@@ -1,44 +1,39 @@
-package at.fhj.iit;
-
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SimpleDrinkTest {
+@DisplayName("Testing SimpleDrink implementation")
+public class SimpleDrinkTest {
 
-    private Liquid milk;
+	private SimpleDrink lA, lN;
 
-    /**
-     * creating a Simpledrink object
-     */
-    @BeforeEach
-    public void setup() {
-        SimpleDrink milkShake = new SimpleDrink("Milk", milk);
+	/**
+	 * inits an alcoholic and nonalcoholic liquid for EACH test
+	 */
+	@BeforeEach
+	void setup() {
+		// SETUP PHASE
+		lN = new SimpleDrink("Wasser", new Liquid("Wasser", 1.0, 0));
+		lA = new SimpleDrink("Bier", new Liquid("Bier", 0.5, 7.5));
 
-    }
+	}
 
+	@Test
+	@DisplayName("Testing constructor non alcoholic")
+	public void testConstructorNonAlcoholic() {
+		assertEquals("Wasser", lN.getName());
+		assertEquals(0, lN.getAlcoholPercent());
+		assertEquals(1.0, lN.getVolume());
+	}
 
-    /**
-     * Test getVolume method
-     */
-    @Test
-    void getVolume() {
-    }
+	@Test
+	@DisplayName("Testing constructor alcoholic")
+	public void testConstructorALcoholic() {
+		assertEquals("Bier", lA.getName());
+		assertEquals(7.5, lA.getAlcoholPercent());
+		assertEquals(0.5, lA.getVolume());
+	}
 
-    /**
-     * Test getAlcoholPercent method
-     */
-    @Test
-    void getAlcoholPercent() {
-    }
-
-    /**
-     * Test isAlcohlic method
-     */
-    @Test
-    void isAlcoholic() {
-    }
 }
